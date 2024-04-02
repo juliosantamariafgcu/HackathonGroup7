dfe01662a34b70335845f4f81cc00c76afdf62dc
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-02 19:34:59 -0400

    testing adding in home page again

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -9,7 +9,9 @@ export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-500 p-4 md:h-52">
-        <PrivatePTOLogo />
+        <p className={`${lusitana.className} text-xl text-white-800 md:text-3xl md:leading-normal`}>
+          <strong>PrivatePTO</strong>
+        </p>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
