a5a62e4e7cf26ad1740397431c9017edf15cee56
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-02 19:26:30 -0400

    testing adding in home page

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -14,18 +14,22 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to PrivatePTO.</strong> This is the example for the{' '}
+            <strong>Welcome to PrivatePTO.</strong> Where Employees can manage and request thier PTO hours,
+            pick up free shifts, and Managers can view and manage incoming PTO requests all in one place!
+            {/* This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-red-500">
-              Next.js Learn Course
-            </a>
+              Next.js Learn Course a>*/}
             , brought to you by Vercel.
           </p>
-          <div className={styles.shape} />
           <Link
               href="/login"
-              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
+              className="flex items-center gap-5 self-start rounded-lg bg-red-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base" >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+          <Link
+              href="/register"
+              className="flex items-center gap-5 self-start rounded-lg bg-red-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base" >
+            <span>Register</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
