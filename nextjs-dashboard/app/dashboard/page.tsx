7c41ffaf0a34014cd8c7c20c82ff6f46ab069d93
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-02 19:05:38 -0400

    PTO added in dashboard

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -12,10 +12,7 @@ export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const {
-        numberOfInvoices,
-        numberOfCustomers,
         totalPaidInvoices,
-        totalPendingInvoices,
     } = await fetchCardData();
 
     return (
@@ -23,9 +20,8 @@ export default async function Page() {
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
                 Dashboard
             </h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-2">
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-1">
                 <Card title="Your PTO" value={totalPaidInvoices} type="collected" />
-                <Card title="Number of People Requesting Time Off" value={totalPendingInvoices} type="pending" />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                  <RevenueChart revenue={revenue}  />
