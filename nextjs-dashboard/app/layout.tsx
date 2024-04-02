e6585c5f4bb7fe70aa4c3a790c4e5f30ab67c681
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-02 17:59:21 -0400

    initial functionality added

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -2,8 +2,8 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
-                                     children,
-                                   }: {
+    children,
+}: {
   children: React.ReactNode;
 }) {
   return (
