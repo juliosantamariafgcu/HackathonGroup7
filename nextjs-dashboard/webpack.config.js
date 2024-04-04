317c2c381d632339b4d5edb002aff9f62271dfff
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-03 23:04:42 -0400

    added html loader for html files with rule

--- nextjs-dashboard/webpack.config.js
+++ nextjs-dashboard/webpack.config.js
@@ -1,9 +1,12 @@
 module.exports = {
+  // Other webpack configurations...
 
   module: {
     rules: [
+      // Other rules...
       {
         test: /\.html$/,
+        exclude: /index\.html$/,  // Exclude index.html from being processed
         use: {
           loader: 'html-loader',
         },
@@ -11,3 +14,4 @@ module.exports = {
     ],
   },
 };
+
