74988a322d3658262a92dfc0e6f39d370747815d
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-05 13:20:07 -0400

    Slight changes to structure of the request/create page.

--- nextjs-dashboard/app/ui/request/create/page.tsx
+++ nextjs-dashboard/app/ui/request/create/page.tsx
@@ -2,13 +2,9 @@ import Form from '@/app/ui/request/create-form';
 import Breadcrumbs from '@/app/ui/request/breadcrumbs';
 import { fetchEmployees } from '@/app/lib/data';
 import { AnyEmployee } from '@/app/lib/definitions';
-import { NextPage } from 'next';
+export async function Page() {
+  const employee: AnyEmployee[] = await fetchEmployees();
 
-interface Props {
-  employee: AnyEmployee[];
-}
-
-const CreateRequestPage: NextPage<Props> = ({ employee }) => {
   return (
     <main>
       <Breadcrumbs
@@ -24,17 +20,5 @@ const CreateRequestPage: NextPage<Props> = ({ employee }) => {
       <Form employee={employee} />
     </main>
   );
-};
-
-export default CreateRequestPage;
-
-export async function getServerSideProps() {
-  const employee: AnyEmployee[] = await fetchEmployees();
-
-  return {
-    props: {
-      employee,
-    },
-  };
 }
 
