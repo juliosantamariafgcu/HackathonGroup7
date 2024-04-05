9dc9b2160a94e5d94b7e17c6809d037a0eddd58e
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2024-04-05 10:59:24 -0400

    Repurpose `InvoiceStatus` into `RequestStatus`
    Additionally make the type of `status` more precise.

--- nextjs-dashboard/app/ui/invoices/status.tsx
+++ nextjs-dashboard/app/ui/invoices/status.tsx
@@ -1,27 +1,39 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { Request } from '@/app/lib/definitions';
+import { CheckIcon, ClockIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
-export default function InvoiceStatus({ status }: { status: string }) {
+export default function RequestStatus({
+  status,
+}: {
+  status: Request['status'];
+}) {
   return (
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
+          'bg-gray-100 text-gray-500': status === 'Pending',
+          'bg-green-500 text-white': status === 'Approved',
+          'bg-red-500 text-white': status === 'Rejected',
         },
       )}
     >
-      {status === 'pending' ? (
+      {status === 'Pending' ? (
         <>
           Pending
           <ClockIcon className="ml-1 w-4 text-gray-500" />
         </>
       ) : null}
-      {status === 'paid' ? (
+      {status === 'Approved' ? (
         <>
-          Paid
+          Approved
           <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === 'Rejected' ? (
+        <>
+          Rejected
+          <XMarkIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
     </span>
