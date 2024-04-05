f2364d6e63fca89a93264d6dee3b54f0f152b809
Julio Santamaria <jasantamaria0183@eagle.fgcu.edu>
2024-04-05 13:29:10 -0400

    Slight changes to structure of the request/create page.

--- nextjs-dashboard/app/ui/request/create/page.tsx
+++ nextjs-dashboard/app/ui/request/create/page.tsx
@@ -1,9 +1,18 @@
+import React from 'react';
 import Form from '@/app/ui/request/create-form';
 import Breadcrumbs from '@/app/ui/request/breadcrumbs';
 import { fetchEmployees } from '@/app/lib/data';
 import { AnyEmployee } from '@/app/lib/definitions';
-export async function Page() {
-  const employee: AnyEmployee[] = await fetchEmployees();
+
+export default async function Page() {
+  let employee: AnyEmployee[] = [];
+
+  try {
+    employee = await fetchEmployees();
+  } catch (error) {
+    console.error('Error fetching employees:', error);
+    // Handle the error, possibly show a message to the user
+  }
 
   return (
     <main>
@@ -12,7 +21,7 @@ export async function Page() {
           { label: 'Request', href: '/dashboard/request' },
           {
             label: 'Create Request',
-            href: '/dashboard/request/create',
+            href: '/dashboard/request',
             active: true,
           },
         ]}
